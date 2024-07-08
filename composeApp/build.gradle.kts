5ca17353ec52927ed619fdd871ca7d2a06faea5c
rwadada <wadada.knct@gmail.com>
2024-07-09 00:37:05 +0900

    add debug setting

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         applicationId = "com.rwadada.taskrecorder"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
     }
     packaging {
         resources {
